09f4613413b5fd9f819a536c63f6b89d4124a2bf
Andrew Feldman <andrew.feldman@microsoft.com>
2020-04-28 19:26:48 -0700

    Working v4 version

--- src/com/microsoft/azure/documentdb/sample/model/TodoItem.java
+++ src/com/microsoft/azure/documentdb/sample/model/TodoItem.java
@@ -1,13 +1,53 @@
 package com.microsoft.azure.documentdb.sample.model;
 
-import lombok.Data;
-import lombok.experimental.Builder;
-
-@Data
-@Builder
+//@Data
+//@Builder
 public class TodoItem {
-    private String category;
+	private String entityType;
+	private String category;
     private boolean complete;
     private String id;
     private String name;
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getEntityType() {
+		return entityType;
+	}
+
+	public void setEntityType(String entityType) {
+		this.entityType = entityType;
+	}
+
+	public boolean isComplete() {
+		return complete;
+	}
+
+	public void setComplete(boolean complete) {
+		this.complete = complete;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+    
+    
 }
